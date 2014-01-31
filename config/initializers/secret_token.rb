92808274134210010d4406b45e0b59831dad99ce
amyg225 <amyg225@gmail.com>
2014-01-31 15:25:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EducationMap::Application.config.secret_key_base = '8b0bd19a6a15bbe56bbd20097d6bbc19bc0d9123c453ad65819f830ab8b853228c96a149794520dad5f6ff58671dbdb4648ee3851cd8bd7f0b5888797a26559a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EducationMap::Application.config.session_store :cookie_store, key: '_education_map_session'
